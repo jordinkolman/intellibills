4755c83f5a1d40e79c16be10996fb31f4c531fcc
Jordin Kolman <jordinkolman@gmail.com>
2025-12-27 13:24:27 -0600

    finished mvp db schema

--- db/schema/20_app.sql
+++ db/schema/20_app.sql
@@ -6,14 +6,19 @@ GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA app TO indibills_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA app
 GRANT ALL PRIVILEGES ON TABLES TO indibills_user;
 
-CREATE TABLE app.rls_admins (
+CREATE TABLE IF NOT EXISTS app.rls_admins (
   id BIGSERIAL PRIMARY KEY,
   db_user text NOT NULL
 );
 
+CREATE OR REPLACE TRIGGER audit_admin_changes
+AFTER INSERT OR UPDATE OR DELETE ON app.rls_admins
+FOR EACH ROW
+EXECUTE FUNCTION audit.log_row_change()
+
 INSERT INTO app.rls_admins (db_user) VALUES ('indibills_user');
 
-CREATE TABLE app.users (
+CREATE TABLE IF NOT EXISTS app.users (
   id BIGSERIAL PRIMARY KEY,
   user_name text NOT NULL,
   password bytea NOT NULL,
@@ -25,3 +30,70 @@ CREATE OR REPLACE TRIGGER audit_user_changes
 AFTER INSERT OR UPDATE OR DELETE ON app.users
 FOR EACH ROW
 EXECUTE FUNCTION audit.log_row_change();
+
+CREATE INDEX idx_users_created_at ON app.users (created_at);
+CREATE INDEX idx_users_updated_at ON app.users (updated_at);
+
+CREATE TABLE IF NOT EXISTS app.institutions (
+  id BIGSERIAL PRIMARY KEY,
+  name text NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+)
+
+CREATE INDEX idx_institution_name ON app.institutions(name);
+
+CREATE TABLE IF NOT EXISTS app.accounts ( 
+  id BIGSERIAL PRIMARY KEY, 
+  user_id BIGINT NOT NULL, 
+  institution_id BIGINT NOT NULL,
+  account_name text NOT NULL, 
+  account_type text NOT NULL, 
+  account_subtype text NOT NULL, 
+  current_balance NUMERIC NOT NULL, 
+  available_balance NUMERIC, 
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  FOREIGN KEY (user_id) REFERENCES app.users (id),
+  FOREIGN KEY (institution_id) REFERENCES app.institutions (id)
+);
+
+CREATE OR REPLACE TRIGGER audit_account_changes
+AFTER INSERT OR UPDATE OR DELETE ON app.accounts
+FOR EACH ROW
+EXECUTE FUNCTION audit.log_row_change();
+
+CREATE INDEX idx_account_account_type ON app.accounts (account_type);
+CREATE INDEX idx_account_account_subtype ON app.accounts (account_subtype);
+CREATE INDEX idx_account_institution_id ON app.accounts (institution_id);
+CREATE INDEX idx_account_created_at ON app.accounts (created_at);
+CREATE INDEX idx_account_udpated_at ON app.accounts (updated_at);
+
+CREATE TABLE IF NOT EXISTS app.transactions (
+  id BIGSERIAL PRIMARY KEY,
+  user_id BIGINT NOT NULL,
+  account_id BIGINT NOT NULL,
+  amount NUMERIC NOT NULL,
+  date DATE NOT NULL,
+  name text NOT NULL,
+  merchant_name text NOT NULL,
+  pending BOOLEAN NOT NULL,
+  category text,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  FOREIGN KEY (user_id) REFERENCES app.users (id),
+  FOREIGN KEY (account_id) REFERENCES app.accounts (id)
+);
+
+CREATE OR REPLACE TRIGGER audit_transaction_changes
+AFTER INSERT OR UPDATE OR DELETE ON app.transactions
+FOR EACH ROW
+EXECUTE FUNCTION audit.log_row_change();
+
+CREATE INDEX idx_transaction_user_id ON app.transactions(user_id);
+CREATE INDEX idx_transaction_account_id ON app.transactions(account_id);
+CREATE INDEX idx_transaction_date ON app.transactions(date);
+CREATE INDEX idx_transaction_merchant_name ON app.transactions(merchant_name);
+CREATE INDEX idx_transaction_category ON app.transactions(category);
+CREATE INDEX idx_transaction_created_at ON app.transactions(created_at);
+CREATE INDEX idx_transactions_updated_at ON app.transactions(updated_at);
