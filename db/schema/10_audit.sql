a0e10a856900d6ff09abf556ff1b7c671843e04c
Jordin Kolman <jordinkolman@gmail.com>
2025-12-30 16:30:50 -0600

    completed audit schema

--- db/schema/10_audit.sql
+++ db/schema/10_audit.sql
@@ -1,73 +1,94 @@
-CREATE SCHEMA audit AUTHORIZATION indibills_owner;
+CREATE SCHEMA audit AUTHORIZATION app_owner;
 
 REVOKE ALL ON SCHEMA audit FROM PUBLIC;
 
-GRANT USAGE ON SCHEMA audit TO indibills_owner;
-GRANT USAGE ON SCHEMA audit TO indibills_user;
+GRANT USAGE ON SCHEMA audit TO app_owner;
 GRANT USAGE ON SCHEMA audit TO auditor;
 
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA audit TO indibills_owner
-GRANT INSERT, SELECT ON ALL TABLES IN SCHEMA audit TO indibills_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA audit TO app_owner;
 GRANT SELECT ON TABLES IN SCHEMA audit TO auditor;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA audit
-GRANT ALL PRIVILEGES ON TABLES TO indbills_owner;
-
-ALTER DEFAULT PRIVILEGES IN SCHEMA audit
-GRANT INSERT, SELECT ON TABLES TO indibills_user;
+GRANT ALL PRIVILEGES ON TABLES TO app_owner;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA audit
 GRANT SELECT ON TABLES TO auditor;
 
 
-CREATE TABLE audit.audit_events(
-  id BIGSERIAL PRIMARY KEY,
+CREATE TABLE audit.audit_event(
+  id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   event_time TIMESTAMPTZ NOT NULL DEFAULT now(),
-  db_user text NOT NULL,
-  operation text NOT NULL,
-  table_name text NOT NULL,
-  query text,
-  session_id text,
-  client_addr inet,
-  source text NOT NULL DEFAULT 'pgaudit'
+  request_id UUID,
+  txid xid8 DEFAULT pg_current_xact_id_if_assigned(),
+  db_role text NOT NULL,
+  user_id UUID,
+  operation TEXT NOT NULL,
+  table_name TEXT NOT NULL,
+  query TEXT NOT NULL,
+  session_id UUID NOT NULL,
+  client_addr inet NOT NULL,
+  source TEXT NOT NULL DEFAULT 'pgaudit',
+  FOREIGN KEY (user_id) REFERENCES core."user"(id),
+  FOREIGN KEY (session_id) REFERENCES auth.session(id)
 );
 
-ALTER TABLE audit.audit_event SET (autovacuum_enables = false);
+CREATE INDEX idx_audit_event_time ON audit.audit_event (event_time);
+CREATE INDEX idx_audit_event_request_id ON audit.audit_event (request_id);
+CREATE INDEX idx_audit_event_user_id ON audit.audit_event (user_id);
 
-CREATE TABLE audit.audit_row_changes(
-  id BIGSERIAL PRIMARY KEY,
+CREATE TABLE audit.audit_row_change(
+  id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   event_time TIMESTAMPTZ NOT NULL DEFAULT now(),
-  table_name text NOT NULL,
-  operation text NOT NULL,
-  db_user text NOT NULL,
+  request_id UUID,
+  txid xid8 DEFAULT pg_current_xact_id_if_assigned(),
+  table_name TEXT NOT NULL,
+  operation TEXT NOT NULL,
+  db_role TEXT NOT NULL,
+  user_id UUID,
   row_pk jsonb,
   before_data jsonb,
-  after_data jsonb
+  after_data jsonb,
+  FOREIGN KEY (user_id) REFERENCES core."user"(id)
 );
 
+CREATE INDEX idx_audit_row_change_event_time ON audit.audit_row_change (event_time);
+CREATE INDEX idx_audit_row_change_request_id ON audit.audit_row_change (request_id);
+CREATE INDEX idx_audit_row_change_user_id ON audit.audit_row_change (user_id);
+
 CREATE OR REPLACE FUNCTION audit.log_row_change()
-RETURNS TRIGGER AS $$
+RETURNS TRIGGER
 SECURITY DEFINER
+SET search_path = audit, public
+AS $$
+DECLARE 
+  v_tenant_id uuid; 
+  v_request_id uuid;
 BEGIN
+IF TG_TABLE_SCHEMA = 'audit' THEN
+  RETURN NULL;
+END IF;
+v_tenant_id := current_setting('app.context.current_tenant_id', true)::uuid;
+v_request_id := current_setting('app.context.request_id', true)::uuid;
 IF TG_OP = 'INSERT' THEN
-INSERT INTO audit.audit_row_changes
-(table_name, operation, db_user, row_pk, after_data) 
+INSERT INTO audit.audit_row_change
+(table_name, request_id, operation, db_role, user_id, row_pk, after_data) 
 VALUES 
-(TG_TABLE_NAME, TG_OP, current_user, to_jsonb(NEW.id), to_jsonb(NEW));
+(TG_TABLE_NAME, v_request_id, TG_OP, current_user, v_tenant_id, to_jsonb(NEW.id), to_jsonb(NEW));
 RETURN NEW;
-ELSIF TG_OP = 'UPDATE' THEN
-INSERT INTO audit.audit_row_changes
-(table_name, operation, db_user, row_pk, before_data, after_data)
+ELSIF TG_OP = 'UPDATE' AND NEW IS DISTINCT FROM OLD THEN
+INSERT INTO audit.audit_row_change
+(table_name, request_id, operation, db_role, user_id, row_pk, before_data, after_data)
 VALUES
-(TG_TABLE_NAME, TG_OP, current_user, to_jsonb(NEW.id), to_jsonb(OLD), to_jsonb(NEW));
+(TG_TABLE_NAME, v_request_id, TG_OP, current_user, v_tenant_id, to_jsonb(NEW.id), to_jsonb(OLD), to_jsonb(NEW));
 RETURN NEW;
 ELSIF TG_OP = 'DELETE' THEN
-INSERT INTO audit.audit_row_changes
-(table_name, operation, db_user, row_pk, before_data)
+INSERT INTO audit.audit_row_change
+(table_name, request_id, operation, db_role, user_id, row_pk, before_data)
 VALUES
-(TG_TABLE_NAME, TG_OP, current_user, to_jsonb(OLD.id), to_jsonb(OLD));
+(TG_TABLE_NAME, v_request_id, TG_OP, current_user, v_tenant_id, to_jsonb(OLD.id), to_jsonb(OLD));
 RETURN OLD;
 END IF;
+RETURN NULL;
 END; $$
 LANGUAGE: PLPGSQL;
 
