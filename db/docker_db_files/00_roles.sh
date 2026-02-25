133f9f62896fd5583cb226aeb4ceb0db6f555a62
Jordin Kolman <jordinkolman@gmail.com>
2026-02-25 15:57:16 -0600

    fixed issue with docker build files

--- db/docker_db_files/00_roles.sh
+++ db/docker_db_files/00_roles.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-if [-z "$APP_RUNTIME_PASSWORD"] || [-z "$APP_WORKER_PASSWORD"] || [-z "$APP_ADMIN_PASSWORD"] || [-z "$AUDITOR_PASSWORD"]; then
+if [ -z "$APP_RUNTIME_PASSWORD" ] || [ -z "$APP_WORKER_PASSWORD" ] || [ -z "$APP_ADMIN_PASSWORD" ] || [ -z "$AUDITOR_PASSWORD" ]; then
   echo "Error: One or more role passwords are not set in the current environment."
   exit 1
 fi
 
-psql -v ON_ERROR_STOP=1 --username = "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" << EOSQL
   -- schema, table, and trigger owner; cannot login
   CREATE ROLE app_owner NOLOGIN;
 
