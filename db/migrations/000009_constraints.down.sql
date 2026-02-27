63545ffa3d364637ab3189aa244269996c51bcdc
Jordin Kolman <jordinkolman@gmail.com>
2026-02-26 19:56:56 -0600

    fixed existance check on tables in constraint down file

--- db/migrations/000009_constraints.down.sql
+++ db/migrations/000009_constraints.down.sql
@@ -1,7 +1,7 @@
 SET ROLE app_owner;
 
-ALTER TABLE audit.audit_row_change DROP CONSTRAINT IF EXISTS fk_audit_row_change_user;
+ALTER TABLE IF EXISTS audit.audit_row_change DROP CONSTRAINT IF EXISTS fk_audit_row_change_user;
 
-ALTER TABLE audit.audit_event DROP CONSTRAINT IF EXISTS fk_audit_event_session;
+ALTER TABLE IF EXISTS audit.audit_event DROP CONSTRAINT IF EXISTS fk_audit_event_session;
 
-ALTER TABLE audit.audit_event DROP CONSTRAINT IF EXISTS fk_audit_event_user;
+ALTER TABLE IF EXISTS audit.audit_event DROP CONSTRAINT IF EXISTS fk_audit_event_user;
